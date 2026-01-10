ee0c33440ad9d7a805d04e8baee9ea9cce10a4f4
Sudha Verma <iamsudhavermaa@gmail.com>
2026-01-10 08:47:01 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.https://chatify-0xsv.onrender.com,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
