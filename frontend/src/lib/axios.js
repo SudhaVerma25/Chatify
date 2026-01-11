48c7757adcc4b14aac2e0e0fe4b0277d144cb451
Sudha Verma <iamsudhavermaa@gmail.com>
2026-01-11 14:20:32 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BACKEND_URL
 export const axiosInstance = axios.create({
   baseURL: BASE_URL + "/api",
-  withCredentials: true,
+  // withCredentials: true,
 });
