258d58bcabb7066623e0b5e1e8830e86379f0e1f
Sudha Verma <iamsudhavermaa@gmail.com>
2026-01-12 01:22:37 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,9 +14,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite:  process.env.NODE_ENV === "production" ? "none" : "lax", // CSRF attacks
-    secure: process.env.NODE_ENV === "production", 
-      // ? false :true,
+    sameSite: "strict", // CSRF attacks
+    secure: ENV.NODE_ENV === "development", ? false :true,
    
   });
 
