13f6fb485fd99fcae44269e3312a69d1d9769009
Sudha Verma <iamsudhavermaa@gmail.com>
2026-01-12 11:34:19 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -15,8 +15,8 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
     sameSite: "none", // CSRF attacks
-    secure: process.env.NODE_ENV === "development" ? false :true,
-   
+    // secure: process.env.NODE_ENV === "development" ? false :true,
+    secure: true,
   });
 
   return token;
