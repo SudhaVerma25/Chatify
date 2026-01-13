a8e7699e57c40def72c3d9ae087af35b4fda17a5
Sudha Verma <iamsudhavermaa@gmail.com>
2026-01-13 18:54:34 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "5mb" })); // req.body
 // app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(
   cors({
-    origin: "https://chatify-frontend-sgdn.onrender.com",
+    origin: "https://chatify-frontend-3byb.onrender.com",
     credentials: true,
   })
 );
