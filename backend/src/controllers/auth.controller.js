f0f192d5f52067129f1e5f7d0ac0f175bab8348e
Sudha Verma <iamsudhavermaa@gmail.com>
2026-01-13 17:18:43 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -97,7 +97,13 @@ export const login = async (req, res) => {
 };
 
 export const logout = (_, res) => {
-  res.cookie("jwt", "", { maxAge: 0 });
+  // res.cookie("jwt", "", { maxAge: 0 });
+   res.cookie("jwt", "", {
+       httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 0,
+  });
   res.status(200).json({ message: "Logged out successfully" });
 };
 
